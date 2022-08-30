a0bd2d0a828e5c519a4d0fd86ae09781ee58e7c4
Brandi Cameron <hello@brandicameron.com>
2022-08-30 11:58:10 -0500

    Added functionality where users can choose difficulty of questions

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import StartPage from './components/StartPage/StartPage';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [difficulty, setDifficulty] = useState('easy');
   const [startGame, setStartGame] = useState(false);
 
-  const handleStartGame = () => {
-    setStartGame(true);
+  const handleStartGame = async () => {
+    try {
+      setLoading(true);
+      const url = `https://opentdb.com/api.php?amount=20&category=9&difficulty=${difficulty}&type=multiple`;
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error(`Error! status: ${response.status}`);
+      }
+
+      const result = await response.json();
+      setData(result);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+      setStartGame(true);
+    }
   };
+
+  useEffect(() => {
+    if (data.results) {
+      console.log(data.results);
+    }
+  }, [data.results]);
 
   return (
     <main className='container'>
-      {!startGame && <StartPage handleStartGame={handleStartGame} />}
+      {!startGame && <StartPage setDifficulty={setDifficulty} handleStartGame={handleStartGame} />}
     </main>
   );
 }
--- src/components/StartPage/StartPage.js
+++ src/components/StartPage/StartPage.js
@@ -3,19 +3,23 @@ import Logo from '../../images/logo.svg';
 import ChoiceButton from '../ChoiceButton/ChoiceButton';
 import ActionButton from '../ActionButton/ActionButton';
 
-export default function StartPage({ handleStartGame }) {
+export default function StartPage({ setDifficulty, handleStartGame }) {
+  const handleSetDifficulty = (e) => {
+    const choice = e.target.textContent;
+    setDifficulty(choice);
+  };
   return (
     <>
       <img src={Logo} alt='Quiz Logo' className={styles.logo} />
       <ul>
         <li>
-          <ChoiceButton label='easy' />
+          <ChoiceButton label='easy' clickHandler={handleSetDifficulty} />
         </li>
         <li>
-          <ChoiceButton label='medium' />
+          <ChoiceButton label='medium' clickHandler={handleSetDifficulty} />
         </li>
         <li>
-          <ChoiceButton label='hard' />
+          <ChoiceButton label='hard' clickHandler={handleSetDifficulty} />
         </li>
       </ul>
       <ActionButton label='play' clickHandler={handleStartGame} />
