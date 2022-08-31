b656cebd0fa7bacb6ec7c82cb57c6a30f8d54827
Brandi Cameron <hello@brandicameron.com>
2022-08-31 15:22:46 -0500

    Handled tracking and color indication of correct/incorrect user answers

--- src/components/ChoiceButton/ChoiceButton.js
+++ src/components/ChoiceButton/ChoiceButton.js
@@ -2,9 +2,10 @@ import styles from './ChoiceButton.module.css';
 import { useState, useEffect, useContext } from 'react';
 import { Context } from '../../Context';
 
-export default function ChoiceButton({ label, clickHandler }) {
+export default function ChoiceButton({ label, clickHandler, userAnswer, correctAnswer }) {
   const { difficulty } = useContext(Context);
   const [buttonColor, setButtonColor] = useState('var(--white)');
+  const [buttonOutlineColor, setButtonOutlineColor] = useState('none');
 
   useEffect(() => {
     if (difficulty === label) {
@@ -12,11 +13,26 @@ export default function ChoiceButton({ label, clickHandler }) {
     } else setButtonColor('var(--white)');
   }, [difficulty]);
 
+  useEffect(() => {
+    if (userAnswer) {
+      if (correctAnswer === label) {
+        setButtonOutlineColor('var(--correct)');
+      }
+
+      if (userAnswer === correctAnswer && userAnswer === label) {
+        setButtonColor('var(--correct)');
+      } else if (userAnswer !== correctAnswer && userAnswer === label) {
+        setButtonColor('var(--incorrect)');
+      }
+    }
+  }, [userAnswer]);
+
   return (
     <button
+      disabled={userAnswer ? true : false}
       className={styles.choiceButton}
       onClick={clickHandler}
-      style={{ backgroundColor: buttonColor }}
+      style={{ backgroundColor: buttonColor, border: `4px solid ${buttonOutlineColor}` }}
     >
       {label}
     </button>
